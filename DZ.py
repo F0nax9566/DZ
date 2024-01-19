db4594e01226daaba941ecf7ee0f2bb99b18cd63
F0nax9566 <120938252+F0nax9566@users.noreply.github.com>
2024-01-19 18:37:56 +0200

    Add files via upload

--- DZ.py
+++ DZ.py
@@ -1,72 +1,20 @@
-import random
+import time
+
+def measure_time(func, *args, **kwargs):
+    start_time = time.time()
+    func(*args, **kwargs)
+    end_time = time.time()
+    return end_time - start_time
+
+def test_measure_time():
+    def sample_function():
+        time.sleep(1)
+
+    result = measure_time(sample_function)
+    print(f"Test 1: {'Passed' if 0.9 < result < 1.1 else 'Failed'}, Time: {result:.2f} seconds")
+
+    def add_numbers(a, b):
+        return a + b
 
 
-class Student:
-    def __init__(self, name):
-        self.name = name
-        self.gladness = 50
-        self.progress = 0
-        self.alive = True
-        self.money = 1000
-
-    def to_work(self):
-        print("Time to work")
-        self.money += 10
-
-    def to_study(self):
-        print("Time to study")
-        self.progress += 0.12
-        self.gladness -= 5
-
-    def to_sleep(self):
-        print("I will sleep")
-        self.gladness += 3
-
-    def to_chill(self):
-        print("Rest time")
-        self.gladness += 5
-        self.progress -= 0.1
-        self.money -= 100
-
-    def is_alive(self):
-        if self.progress < -0.5:
-            print("Cast out…")
-            self.alive = False
-        elif self.gladness <= 0:
-            print("Depression…")
-            self.alive = False
-        elif self.progress > 5:
-            print("Passed externally…")
-            self.alive = False
-
-    def end_of_day(self):
-        print(f"Gladness = {self.gladness}")
-        print(f"Progress ={round(self.progress, 2)}")
-        print(f"Money = {round(self.money, 2)}")
-
-    def live(self, day):
-        day = "Day" + str(day) + "of" + self.name + "life"
-        print(f"{day:=^50}")
-        live_cube = random.randint(1, 3)
-        if live_cube == 1:
-            self.to_study()
-        elif live_cube == 2:
-            self.to_sleep()
-        elif live_cube == 3:
-            self.to_chill()
-        self.end_of_day()
-        self.is_alive()
-        if self.money < 100:
-            print("Go to work")
-            self.to_work()
-
-
-# ==============================================
-
-
-nick = Student(name="Nick")  # __init__
-
-for day in range(365):
-    if nick.alive == False:
-        break
-    nick.live(day)
+test_measure_time()
